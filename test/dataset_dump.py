ef910ea1f773bde9de8e140e7ace18d1485b994e
Maksim Skazkin <makcymal@yandex.ru>
2025-01-09 00:09:35 +1000

    rm: test utilities
    Tests are still available in devel branch

--- test/dataset_dump.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import pyarrow as pa
-
-df = pd.read_csv("dataset.csv")
-table = pa.Table.from_pandas(df)
-with pa.ipc.new_file("dataset.arrow", table.schema) as writer:
-  writer.write(table)
